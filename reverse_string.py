98d812a0bc52e81610b08c8b6b46b823e03fc88b
Penelope Hill <penelopehill@Penelopes-MacBook-Pro-2.local>
2015-02-04 21:32:48 -0800

    Adding new exercises

--- reverse_string.py
+++ reverse_string.py
@@ -10,4 +10,18 @@ def reverse_string(stringy):
 
 stringy = "Sunshine on a rainy day"
 
-print reverse_string(stringy)
+# print reverse_string(stringy)
+
+
+ 
+def reverse_words(stringy): 
+    reversed_stringy = ""
+    new_string = stringy.split()
+    word_list = list(new_string)
+
+    for i in range(len(word_list)): 
+        temp = word_list[-1 - i]
+        reversed_stringy += temp + " "
+    return reversed_stringy
+
+print reverse_words("hello world dog charlie")
